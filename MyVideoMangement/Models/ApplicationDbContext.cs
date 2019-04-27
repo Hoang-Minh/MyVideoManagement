b651c365df8222e73f7e5463f65ababea6350975
Hoang-Minh <quataoxanh83@gmail.com>
2019-04-27 09:07:20 -0700

    Extract view model classes

--- MyVideoMangement/Models/ApplicationDbContext.cs
+++ MyVideoMangement/Models/ApplicationDbContext.cs
@@ -1,18 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using System.Data.Entity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MyVideoMangement.Models
 {
-    //// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    //public class ApplicationUser : IdentityUser
-    //{
-        
-    //}
-
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
